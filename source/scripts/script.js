210d8d3be096e7b8dd7378da5018ef723697992c
Georgij Tsabiev <gtsabiev@inbox.ru>
2023-11-27 21:18:33 +0300

    Добавляет открытие и закрытие меню навигации

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+/* Мобильная навигация */
+
+let nav = document.querySelector('.nav');
+let navToggle = document.querySelector('.nav__toggle');
+let navList = document.querySelector('.nav__list');
+
+nav.classList.remove("nav--no-js");
+navToggle.classList.remove('nav__toggle--no-js');
+
+navToggle.addEventListener('click', function () {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+});
