c7b3bafffe36c44ba74a0a9e7c64aeec5ebfec8b
Georgij Tsabiev <gtsabiev@inbox.ru>
2023-11-27 21:27:57 +0300

    Исправляет js код по stylelint

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,13 +1,12 @@
 /* Мобильная навигация */
 
-let nav = document.querySelector('.nav');
-let navToggle = document.querySelector('.nav__toggle');
-let navList = document.querySelector('.nav__list');
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
 
-nav.classList.remove("nav--no-js");
+nav.classList.remove('nav--no-js');
 navToggle.classList.remove('nav__toggle--no-js');
 
-navToggle.addEventListener('click', function () {
+navToggle.addEventListener('click', () => {
   if (nav.classList.contains('nav--closed')) {
     nav.classList.remove('nav--closed');
     nav.classList.add('nav--opened');
